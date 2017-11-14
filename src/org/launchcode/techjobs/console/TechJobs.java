ffa0e75fe9dd65ed0d22961bd412a7cc3115eb9e
Lisa Foreman <lisa-foreman@outlook.com>
2017-11-13 19:03:10 -0600

    Final commit

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -61,7 +62,7 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -110,7 +111,21 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.size() == 0) {
+            System.out.println("No jobs to print");
+            return;
+        }
 
-        System.out.println("printJobs is not implemented yet");
+
+        for (HashMap<String, String> job : someJobs) {
+            String jobInfo = "\n************\n";
+
+            for (Map.Entry<String, String> jobColumn : job.entrySet()) {
+                jobInfo += jobColumn.getKey() + " : " + jobColumn.getValue() + "\n";
+            }
+
+            jobInfo += "\n************\n";
+            System.out.println(jobInfo);
+        }
     }
 }
